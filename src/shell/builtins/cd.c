f3ca1f824f7852d9f318705d2ed4737430ffae48
Gabriel Ravier <gabravier@gmail.com>
2021-01-31 13:18:26 +0100

    Fixed cd to be as close as I can make it to tcsh cd
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- src/shell/builtins/cd.c
+++ src/shell/builtins/cd.c
@@ -6,17 +6,27 @@
 */
 
 #include "functions.h"
+#include "../../error.h"
 #include "my/stdlib.h"
+#include "my/string.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stddef.h>
 
 int shell_builtin_cd(struct shell *self)
 {
+    char *current_working_directory = getcwd(NULL, 0);
+    char *destination_from_arg = self->arguments[1] == NULL ? NULL : my_strcmp(self->arguments[1], "-") ? (my_getenv("owd") ?: "") : self->arguments[1];
+
+    if (destination_from_arg)
+        if (chdir(destination_from_arg) != 0)
+            perror(destination_from_arg);
     if ((self->arguments[1] != NULL || my_getenv("HOME") != NULL) &&
         (chdir(self->arguments[1] ?: my_getenv("HOME")) != 0)) {
-        perror("cd");
+        error("cd: Can't change to home directory.");
         return (1);
     }
+    my_setenv("owd", current_working_directory, 1);
+    free(current_working_directory);
     return (0);
 }
