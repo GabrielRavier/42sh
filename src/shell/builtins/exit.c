dfc03d9c069e10159686b9e12f45642e65505cd8
Gabriel Ravier <gabravier@gmail.com>
2021-01-31 13:18:03 +0100

    Fixed exit to take into account the given argument
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- src/shell/builtins/exit.c
+++ src/shell/builtins/exit.c
@@ -6,10 +6,11 @@
 */
 
 #include "functions.h"
+#include "my/stdlib.h"
 #include <stdlib.h>
 #include <sys/wait.h>
 
 int shell_builtin_exit(struct shell *self)
 {
-    exit(0);
+    exit(self->arguments[1] ? my_strtol(self->arguments[1], NULL, 10) : 0);
 }
