9decd86eb75500490b1a34a41c1cb575c4058837
Gabriel Ravier <gabravier@gmail.com>
2021-01-31 13:06:40 +0100

    Fixed setenv not erroring when there are too many arguments
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- src/shell/builtins/setenv.c
+++ src/shell/builtins/setenv.c
@@ -12,21 +12,32 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int shell_builtin_setenv(struct shell *self)
+static bool check_args(struct shell *self)
 {
-    if (self->arguments[1] == NULL)
-        return (shell_builtin_env(self));
+    if (self->arguments[1] && self->arguments[2] && self->arguments[3]) {
+        error("setenv: Too many arguments.");
+        return (false);
+    }
     if (!my_isalpha(self->arguments[1][0])) {
         error("setenv: Variable name must begin with a letter.");
-        return (1);
+        return (false);
     }
     for (size_t i = 0; self->arguments[1][i]; ++i)
         if (!my_isalnum(self->arguments[1][i]) &&
             self->arguments[1][i] != '_') {
             error(
                 "setenv: Variable name must contain alphanumeric characters.");
-            return (1);
+            return (false);
         }
+    return (true);
+}
+
+int shell_builtin_setenv(struct shell *self)
+{
+    if (self->arguments[1] == NULL)
+        return (shell_builtin_env(self));
+    if (!check_args(self))
+        return (1);
     if (my_setenv(self->arguments[1], self->arguments[2] ?: "", 1) < 0) {
         perror("setenv");
         return (1);
