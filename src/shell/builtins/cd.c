0679815a97eaa5c59697d1e96035c11bf81203f1
Gabriel Ravier <gabravier@gmail.com>
2021-01-31 13:46:51 +0100

    Hopefully fully fixed up cd
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- src/shell/builtins/cd.c
+++ src/shell/builtins/cd.c
@@ -16,27 +16,37 @@
 #include <errno.h>
 #include <stddef.h>
 
-int shell_builtin_cd(struct shell *self)
+static bool do_it(struct shell *self, const char *destination_from_arg)
 {
-    char *current_working_directory = getcwd(NULL, 0);
-    char *destination_from_arg = self->arguments[1] == NULL ? NULL :
-        (my_strcmp(self->arguments[1], "-") == 0) ? (my_getenv("owd") ?: "") :
-        self->arguments[1];
-
     if (destination_from_arg) {
         if (chdir(destination_from_arg) != 0) {
             my_dprintf(STDERR_FILENO, "%s: %s.\n", destination_from_arg,
                 strerror(errno));
-            free(current_working_directory);
-            return (1);
+            return (false);
         }
     } else if ((self->arguments[1] != NULL || my_getenv("HOME") != NULL) &&
         (chdir(self->arguments[1] ?: my_getenv("HOME")) != 0)) {
         error("cd: Can't change to home directory.");
+        return (false);
+    }
+    return (true);
+}
+
+int shell_builtin_cd(struct shell *self)
+{
+    char *current_working_directory = getcwd(NULL, 0);
+    const char *destination_from_arg = self->arguments[1] == NULL ? NULL :
+        (my_strcmp(self->arguments[1], "-") == 0) ? (my_getenv("owd") ?: "") :
+        self->arguments[1];
+
+    if (!do_it(self, destination_from_arg)) {
         free(current_working_directory);
         return (1);
     }
     my_setenv("owd", current_working_directory, 1);
+    free(current_working_directory);
+    current_working_directory = getcwd(NULL, 0);
+    my_setenv("PWD", current_working_directory, 1);
     free(current_working_directory);
     return (0);
 }
