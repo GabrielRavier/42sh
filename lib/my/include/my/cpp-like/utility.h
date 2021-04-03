546554fafb8e7305bdb1b7353a43dd19c8fa7b6a
Gabriel Ravier <gabravier@gmail.com>
2021-04-04 00:04:39 +0200

    libmy: Made MY_SWAP avoid getting a warning with -Wpedantic

--- lib/my/include/my/cpp-like/utility.h
+++ lib/my/include/my/cpp-like/utility.h
@@ -11,8 +11,8 @@
 
 /// Exchanges the given values
 #define MY_SWAP(a, b) \
-    do { \
+    do { __extension__ ({ \
         __auto_type MY_MAKE_UNIQUE_IDENTIFIER(_swap_tmp) = (a); \
         (a) = (b); \
         (b) = MY_MAKE_UNIQUE_IDENTIFIER(_swap_tmp); \
-    } while (0)
+    });} while (0)
