cfa72228c4b86c4ab2a371d8cefe877ef73952a0
Gabriel Ravier <gabravier@gmail.com>
2021-05-10 01:20:01 +0200

    libmy: Fixed vdprintf to try and write as much as possible

--- lib/my/src/stdio/printf_my_string_based/vdprintf.c
+++ lib/my/src/stdio/printf_my_string_based/vdprintf.c
@@ -8,16 +8,31 @@
 #include "my/stdio.h"
 #include "my/stdlib.h"
 #include "my/unistd.h"
+#include <stdbool.h>
+
+static bool do_write(int fd, const char *data, ssize_t n)
+{
+    ssize_t write_result;
+
+    while (n > 0) {
+        write_result = my_write(fd, data, n);
+        if (write_result < 0)
+            return false;
+        n -= write_result;
+        data += write_result;
+    }
+    return true;
+}
 
 int my_vdprintf(int fd, const char *format, va_list arguments)
 {
     char *string_to_print;
     const int string_length = my_vasprintf(&string_to_print, format, arguments);
-    int result;
+    bool result;
 
     if (string_length < 0)
         return -1;
-    result = (int)my_write(fd, string_to_print, (size_t)string_length);
+    result = do_write(fd, string_to_print, string_length);
     my_free(string_to_print);
-    return result;
+    return result ? string_length : -1;
 }
