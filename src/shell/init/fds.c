a5b7e69ca3f49e822691f11b19eb71f2270096ee
Gabriel Ravier <gabravier@gmail.com>
2021-05-14 19:43:25 +0200

    Made it so that the shell fds are closed on exec

--- src/shell/init/fds.c
+++ src/shell/init/fds.c
@@ -8,13 +8,20 @@
 #include "../init.h"
 #include "../../fd.h"
 #include <unistd.h>
+#include <fcntl.h>
+
+static void do_one(int *result, int src, int dest)
+{
+    *result = fd_copy(src, dest);
+    fcntl(*result, F_SETFD, FD_CLOEXEC);
+}
 
 void shell_init_fds(struct shell *self)
 {
     self->child_io_fds_setup = false;
-    self->input_fd = fd_copy(STDIN_FILENO, SHELL_PREFERRED_INPUT_FD);
-    self->output_fd = fd_copy(STDOUT_FILENO, SHELL_PREFERRED_OUTPUT_FD);
-    self->error_output_fd = fd_copy(STDERR_FILENO,
+    do_one(&self->input_fd, STDIN_FILENO, SHELL_PREFERRED_INPUT_FD);
+    do_one(&self->output_fd, STDOUT_FILENO, SHELL_PREFERRED_OUTPUT_FD);
+    do_one(&self->error_output_fd, STDERR_FILENO,
         SHELL_PREFERRED_ERROR_OUTPUT_FD);
-    self->old_stdin_fd = fd_copy(self->input_fd, SHELL_PREFERRED_OLD_STDIN_FD);
+    do_one(&self->old_stdin_fd, self->input_fd, SHELL_PREFERRED_OLD_STDIN_FD);
 }
