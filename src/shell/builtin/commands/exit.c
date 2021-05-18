5fe6cf7cdda36a9bf2403eac989c932d5c6dbdee
Gabriel Ravier <gabravier@gmail.com>
2021-05-19 01:36:24 +0200

    Slightly tweaked exit and added a bunch of tests for it
    Changes:
    - exit no longer directly exits but instead sets
      last_command_exit_status, and does not touch it if there are no
      arguments
    - Changed shell_builtin_run to use shell_char_strv_len instead of
      doing it manually

--- src/shell/builtin/commands/exit.c
+++ src/shell/builtin/commands/exit.c
@@ -6,14 +6,15 @@
 */
 
 #include "../commands.h"
-#include "../../exit.h"
+#include "../../input_set_eof.h"
 #include "my/stdlib.h"
-#include <stdlib.h>
-#include <sys/wait.h>
+#include <unistd.h>
 
 void shell_builtin_exit(struct shell *self, shell_char_t **argv)
 {
-    (void)self;
-    shell_exit(self, argv[1] ? my_strtol(shell_char_xstrdup_to_c(argv[1]),
-        NULL, 10) : 0);
+    if (argv[1] != NULL)
+        self->last_command_exit_status = my_strtol(
+            shell_char_static_xstrdup_to_c(argv[1]), NULL, 10);
+    shell_input_set_eof(self);
+    close(self->input_fd);
 }
--- src/shell/builtin/run.c
+++ src/shell/builtin/run.c
@@ -12,18 +12,13 @@
 void shell_builtin_run(struct shell *self, const struct shell_builtin *builtin,
     struct shell_parse_tree *parse_tree)
 {
-    size_t args = -1;
+    size_t arg_count = shell_char_strv_len_ncnst(parse_tree->argv) - 1;
 
     self->error_program_name = builtin->name;
-    for (shell_char_t **i = parse_tree->argv; *i != NULL; ++i)
-        ++args;
-    if (args < builtin->min_arguments)
+    if (arg_count < builtin->min_arguments)
         shell_set_error(self, SHELL_ERROR_TOO_FEW_ARGUMENTS);
-    if (args > builtin->max_arguments)
+    if (arg_count > builtin->max_arguments)
         shell_set_error(self, SHELL_ERROR_TOO_MANY_ARGUMENTS);
-    if (!self->error) {
+    if (!self->error)
         builtin->func(self, parse_tree->argv);
-        self->last_command_exit_status = (self->error != NULL);
-    } else
-        shell_print_error(self);
 }
